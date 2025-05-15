a302a664623f75c7638184c6735ce12c934dd7e0
mncnf <mncnf0113@gmail.com>
2025-05-16 04:55:18 +0900

    Button

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -9,23 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello")
-                .font(.largeTitle)
-            Text("Hello")
-                .font(.headline)
-            Text("Hello")
-                .font(.body)
-            Text("Hello")
-                .font(.callout)
-            Text("Hello")
-                .font(.subheadline)
-            Text("Hello")
-                .font(.footnote)
-            Text("Hello")
-                .font(.caption)
+        Button(action: {
+            print("Pushed Button!!!!")
+        }) {
+            Text("Push Button!")
         }
     }
+}
+
+func Report() {
+    print("Pushed button!")
 }
 
 #Preview {

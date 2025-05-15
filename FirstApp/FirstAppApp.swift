e2a20477b8e87de67bb3436936d9fedf0360e007
mncnf <mncnf0113@gmail.com>
2025-05-16 00:12:50 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Development on 2025/05/16.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

d3c6f80a189cc34c06c8f4f768b147b160543902
mncnf <mncnf0113@gmail.com>
2025-05-16 01:17:45 +0900

    section3-15

--- FirstApp/tmp.playground/Contents.swift
+++ FirstApp/tmp.playground/Contents.swift
@@ -1,4 +1,4 @@
-var number = 290
+var number = 30
 
 if number == 10 {
     print(10)
@@ -7,5 +7,14 @@ else if number == 30 {
     print(30)
 }
 else {
+    print("それ以外")
+}
+
+switch number {
+case 10:
+    print(10)
+case 30:
+    print(30)
+default:
     print("それ以外")
 }

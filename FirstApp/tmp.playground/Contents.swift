89cde478582a9c0ad1a40124f8a5573f5e7327fe
mncnf <mncnf0113@gmail.com>
2025-05-16 01:35:53 +0900

    section3-18

--- FirstApp/tmp.playground/Contents.swift
+++ FirstApp/tmp.playground/Contents.swift
@@ -1,5 +1,25 @@
 //var optionalInt :Int? = 30
 //print(optionalInt)
 
-var optionalInt :Int? = 30
-print(optionalInt!)
+//var optionalInt :Int? = 30
+//print(optionalInt!)
+
+//var optionalInt :Int? = 30
+//var optionalInt :Int? = nil
+//if let unwrapedInt = optionalInt {
+//    print(unwrapedInt)
+//} else {
+//    print("unwrapedIntはnil")
+//}
+
+
+func Unwrap() {
+    var optionalInt :Int? = 20
+    guard let unwrapedInt = optionalInt else {
+        print("unwrapedIntはnil")
+        return
+    }
+    print(unwrapedInt)
+}
+
+Unwrap()

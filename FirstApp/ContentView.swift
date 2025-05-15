5b7145594b06f00ae09587c3ee463fc5aa2550a7
mncnf <mncnf0113@gmail.com>
2025-05-16 04:43:08 +0900

    41

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
+        VStack {
             Text("Hello")
             Text("Hello")
             Text("Hello")

4ae9512d55a2c9c4edd8488113e20b5ef217e7da
mncnf <mncnf0113@gmail.com>
2025-05-16 01:20:59 +0900

    section3-16

--- FirstApp/tmp.playground/Contents.swift
+++ FirstApp/tmp.playground/Contents.swift
@@ -1,20 +1,3 @@
-var number = 30
+var optionalInt :Int? = 30
 
-if number == 10 {
-    print(10)
-}
-else if number == 30 {
-    print(30)
-}
-else {
-    print("それ以外")
-}
-
-switch number {
-case 10:
-    print(10)
-case 30:
-    print(30)
-default:
-    print("それ以外")
-}
+print(optionalInt)

f2ee8d595564a4dfdcb877dc272760888047a220
mncnf <mncnf0113@gmail.com>
2025-05-16 04:45:52 +0900

    42

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -11,8 +11,19 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Hello")
+                .font(.largeTitle)
             Text("Hello")
+                .font(.headline)
             Text("Hello")
+                .font(.body)
+            Text("Hello")
+                .font(.callout)
+            Text("Hello")
+                .font(.subheadline)
+            Text("Hello")
+                .font(.footnote)
+            Text("Hello")
+                .font(.caption)
         }
     }
 }

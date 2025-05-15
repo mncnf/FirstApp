25d5d89605481194dc92b6e58bc27895001a83b2
mncnf <mncnf0113@gmail.com>
2025-05-16 02:30:23 +0900

    section3-26

--- FirstApp/tmp.playground/Contents.swift
+++ FirstApp/tmp.playground/Contents.swift
@@ -13,13 +13,45 @@
 //print(dog.age)
 //print(dog.kind)
 //dog.Bite()
+//
+//class Animal {
+//    var age: Int
+//    let kind: String
+//    
+//    func Bite()
+//    {
+//        print("\(age)歳の\(kind)が噛み付く")
+//    }
+//    
+//    init(age: Int, kind: String) {
+//        self.age = age
+//        self.kind = kind
+//    }
+//}
+//
+//var dog = Animal(age: 2,kind: "犬")
+//var cat = Animal(age: 10,kind: "猫")
+//dog.Bite()
+//cat.Bite()
 
-class Animal {
-    var age: Int
-    let kind: String
+// section3-26
+//struct Animal {
+//    let age = 5
+//    var kind = "犬"
+//    
+//    func Bite() {
+//        print("\(age)歳の\(kind)が噛み付く")
+//    }
+//}
+//
+//var dog = Animal()
+//dog.Bite()
+
+struct Animal {
+    let age: Int
+    var kind: String
     
-    func Bite()
-    {
+    func Bite() {
         print("\(age)歳の\(kind)が噛み付く")
     }
     
@@ -29,7 +61,13 @@ class Animal {
     }
 }
 
-var dog = Animal(age: 2,kind: "犬")
-var cat = Animal(age: 10,kind: "猫")
-dog.Bite()
-cat.Bite()
+var panda = Animal(age: 10, kind: "パンダ")
+panda.Bite()
+
+var animals: [Animal] = [
+    Animal(age: 10, kind: "虎"),
+    Animal(age: 2, kind: "二限"),
+    Animal(age: 1, kind: "へび"),
+]
+
+animals[1].Bite()

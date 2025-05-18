770e782a6e80c401e0b0211890264b8ddce68efb
mncnf <mncnf0113@gmail.com>
2025-05-18 23:57:00 +0900

    ToggleStyle

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var isDisabled = false
+    @State var isMan = true
 
     var body: some View {
-        Button("ボタン") {
-            isDisabled = true
+        Toggle("Toggle", isOn: $isMan)
+            .toggleStyle(GenderToggleStyle())
+    }
+}
+
+struct GenderToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            Button("男性") {
+                configuration.isOn.toggle()
+            }
+            .disabled(configuration.isOn)
+
+            Button("女性") {
+                configuration.isOn.toggle()
+            }
+            .disabled(!configuration.isOn)
         }
-        .disabled(isDisabled)
     }
 }
 
